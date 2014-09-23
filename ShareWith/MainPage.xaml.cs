1691f3012d859f561443cfff425757a3a4bcbef3
hyewon20 <hwon0505@gmail.com>
2014-09-23 23:09:01 +0900

    test sample

--- ShareWith/MainPage.xaml.cs
+++ ShareWith/MainPage.xaml.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -13,18 +12,19 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// 빈 페이지 항목 템플릿에 대한 설명은 http://go.microsoft.com/fwlink/?LinkId=234238에 나와 있습니다.
+// The Basic Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234237
 
 namespace ShareWith
 {
     /// <summary>
-    /// 자체에서 사용하거나 프레임 내에서 탐색할 수 있는 빈 페이지입니다.
+    /// A basic page that provides characteristics common to most applications.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class MainPage : ShareWith.Common.LayoutAwarePage
     {
         public MainPage()
         {
             this.InitializeComponent();
         }
+
     }
 }
