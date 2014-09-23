0f8a77185476f96f83474cc7f2aa93ed0a3d5329
hyewon20 <hwon0505@gmail.com>
2014-09-23 22:53:41 +0900

    add circleTest
    add circleTest

--- ShareWith/MainPage.xaml.cs
+++ ShareWith/MainPage.xaml.cs
@@ -26,10 +26,5 @@ namespace ShareWith
         {
             this.InitializeComponent();
         }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
     }
 }
