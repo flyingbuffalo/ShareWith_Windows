b128de93ad62f97f6832eb62df858e0cff26377c
hyewon20 <hwon0505@gmail.com>
2014-09-24 22:48:57 +0900

    Apply Buffalo Interface
    Apply Buffalo Interface

--- ShareWith/MainPage.xaml.cs
+++ ShareWith/MainPage.xaml.cs
@@ -1,10 +1,12 @@
 ﻿using ShareWith.Common;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
+using Windows.Storage.Streams;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -13,37 +15,44 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using System.Threading.Tasks;
+using System.Threading;
+using Windows.UI.Core;
+using Windows.Networking;
+using Windows.Networking.Sockets;
+using Windows.System.Threading;
+using System.Diagnostics;
+using Buffalo.WiFiDirect;
 
 // 기본 페이지 항목 템플릿에 대한 설명은 http://go.microsoft.com/fwlink/?LinkId=234237에 나와 있습니다.
 
 namespace ShareWith
 {
-    /// <summary>
-    /// 대부분의 응용 프로그램에 공통되는 특성을 제공하는 기본 페이지입니다.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
-
         private NavigationHelper navigationHelper;
         private ObservableDictionary defaultViewModel = new ObservableDictionary();
 
-        /// <summary>
-        /// 이는 강력한 형식의 뷰 모델로 변경될 수 있습니다.
-        /// </summary>
         public ObservableDictionary DefaultViewModel
         {
             get { return this.defaultViewModel; }
         }
 
-        /// <summary>
-        /// NavigationHelper는 각 페이지에서 탐색 및 프로세스 수명 관리를 
-        /// 지원하는 데 사용됩니다.
-        /// </summary>
         public NavigationHelper NavigationHelper
         {
             get { return this.navigationHelper; }
         }
 
+        private WFDManager manager;
+        WFDDevice device;
+        internal WFDPairInfo pairInfo;
+
+        DiscoveredListener discoveredListener = null;
+
+        public TextBlock TxtMessage
+        {
+            get { return txtMessage; }
+        }
 
         public MainPage()
         {
@@ -51,61 +60,99 @@ namespace ShareWith
             this.navigationHelper = new NavigationHelper(this);
             this.navigationHelper.LoadState += navigationHelper_LoadState;
             this.navigationHelper.SaveState += navigationHelper_SaveState;
+
+            discoveredListener = new DiscoveredListener(this);
+            manager = new WFDManager(this, discoveredListener, discoveredListener);
+            
         }
 
-        /// <summary>
-        /// 탐색 중 전달된 콘텐츠로 페이지를 채웁니다. 이전 세션의 페이지를
-        /// 다시 만들 때 저장된 상태도 제공됩니다.
-        /// </summary>
-        /// <param name="sender">
-        /// 대개 <see cref="NavigationHelper"/>인 이벤트 소스
-        /// </param>
-        /// <param name="e">다음에 전달된 탐색 매개 변수를 제공하는 이벤트 데이터입니다.
-        /// <see cref="Frame.Navigate(Type, Object)"/>에 전달된 매개 변수와
-        /// 이전 세션 동안 이 페이지에 유지된
-        /// 유지됩니다. 페이지를 처음 방문할 때는 이 상태가 null입니다.</param>
         private void navigationHelper_LoadState(object sender, LoadStateEventArgs e)
         {
         }
 
-        /// <summary>
-        /// 응용 프로그램이 일시 중지되거나 탐색 캐시에서 페이지가 삭제된 경우
-        /// 이 페이지와 관련된 상태를 유지합니다.  값은
-        /// <see cref="SuspensionManager.SessionState"/>의 serialization 요구 사항을 만족해야 합니다.
-        /// </summary>
-        /// <param name="sender"> 대개 <see cref="NavigationHelper"/>인 이벤트 소스</param>
-        /// <param name="e">serializable 상태로 채워질
-        /// 빈 사전입니다.</param>
         private void navigationHelper_SaveState(object sender, SaveStateEventArgs e)
         {
         }
 
-        #region NavigationHelper 등록
-
-        /// 이 섹션에서 제공되는 메서드는 NavigationHelper에서
-        /// 페이지의 탐색 메서드에 응답하기 위해 사용됩니다.
-        /// 
-        /// 페이지별 논리는 다음에 대한 이벤트 처리기에 있어야 합니다.  
-        /// <see cref="GridCS.Common.NavigationHelper.LoadState"/>
-        /// 및 <see cref="GridCS.Common.NavigationHelper.SaveState"/>입니다.
-        /// 탐색 매개 변수는 LoadState 메서드 
-        /// 및 이전 세션 동안 유지된 페이지 상태에서 사용할 수 있습니다.
-
-        protected override void OnNavigatedTo(NavigationEventArgs e)
-        {
-            navigationHelper.OnNavigatedTo(e);
-        }
-
-        protected override void OnNavigatedFrom(NavigationEventArgs e)
-        {
-            navigationHelper.OnNavigatedFrom(e);
-        }
-
-        #endregion
-
         private void btnDiscovery_Click(object sender, RoutedEventArgs e)
         {
-
+            manager.getDevicesAsync();
+            txtMessage.Text = "Finding Devices...";
         }
     }
+
+
+    public class DiscoveredListener : WFDDeviceDiscoveredListener, WFDDeviceConnectedListener, WFDPairInfo.PairSocketConnectedListener
+    {
+        MainPage parent;
+        public DiscoveredListener(MainPage parent)
+        {
+            this.parent = parent;
+        }
+
+        public async void onDevicesDiscovered(List<WFDDevice> deviceList)
+        {
+            ObservableCollection<WFDDevice> devList = new ObservableCollection<WFDDevice>(deviceList);
+           // parent.comboDeviceList.ItemsSource = devList;
+
+            if (deviceList.Count != 0)
+            {
+                foreach (WFDDevice dev in deviceList)
+                {
+                    Debug.WriteLine(dev.Name);
+                }
+                //parent.comboDeviceList.SelectedIndex = 0;
+                parent.TxtMessage.Text = "Found " + deviceList.Count;
+            }
+            else
+            {
+                parent.TxtMessage.Text = "Found Not";
+            }
+        }
+
+        public void onDevicesDiscoverFailed(int reasonCode)
+        {
+
+        }
+
+
+        //connceted
+        public void onDeviceConnected(WFDPairInfo pair)
+        {
+            parent.pairInfo = pair;
+
+            Debug.WriteLine("paring");
+            parent.TxtMessage.Text = "Device's IP Address : " + pair.getRemoteAddress();
+
+            pair.connectSocketAsync(this);
+        }
+
+        public void onDeviceConnectFailed(int reasonCode)
+        {
+
+        }
+
+        public void onDeviceDisconnected()
+        {
+
+        }
+
+        public void onSocketConnected(StreamSocket s)
+        {
+            parent.TxtMessage.Text = "Socket Connected.";
+
+            DataWriter writer = new DataWriter(s.OutputStream);
+            writer.WriteString("ping~ping~\n");
+
+            writer.StoreAsync();
+            writer.FlushAsync();
+
+            parent.TxtMessage.Text = "Send Message.";
+            //   writer.Dispose();
+            //    s.Dispose();
+
+            // parent.manager.unpair(parent.pairInfo);
+        }
+    }   
+
 }
