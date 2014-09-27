27126767d6fed10928d974439f4f870ab7a083ed
hyewon20 <hwon0505@gmail.com>
2014-09-27 20:31:51 +0900

    Add File Receive UI
    Add File Receive UI

--- ShareWith/MainPage.xaml.cs
+++ ShareWith/MainPage.xaml.cs
@@ -187,9 +187,38 @@ namespace ShareWith
                 
         }
 
-        public void onSocketReceived(StreamSocket s)
+        public async void onSocketReceived(StreamSocket s)
         {
+            BasicProperties fileProperty = await file.GetBasicPropertiesAsync();
+            double fileSize = Convert.ToDouble(fileProperty.Size);
+
             Debug.WriteLine("RECEIVEDD ");
+            StorageFolder folder = await parent.FileSave();
+            Debug.WriteLine("FileSave");
+
+            await parent.ReceiveFileFromPeer(s, folder);
+
+            if (fileProperty.Size != 0)
+            {
+                parent.startProgress();
+
+                await System.Threading.Tasks.Task.Run(async () =>
+                {
+                    for (int i = 0; i <= Math.Ceiling(fileSize) / BLOCK_SIZE; i++)
+                    {
+                        await parent.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.High, () =>
+                        {
+                            parent.setProgressValue(Convert.ToInt32(Math.Ceiling(fileSize) / BLOCK_SIZE * 100));
+                        });
+                        // await System.Threading.Tasks.Task.Delay(5);안쓰면될걸
+                    }
+                });
+            }
+            else
+            {
+                throw new FileNotFoundException("[Exception] : File is null.");
+            }   
+
             //received
         }
 
